3b7382217209af27d962f06c56d2a5273a3baa1a
Miguel Ramos <return.infox@gmail.com>
2025-10-27 14:59:14 -0300

    Mapeo de la clase login

--- pages/login_page.py
+++ pages/login_page.py
@@ -11,3 +11,27 @@ class LoginPage:
     PASS_INPUT = (By.ID, 'pass')
     LOGIN_BUTTON = (By.ID, 'login')
     FEEDBACK = (By.ID, 'feedback')
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    def open(self, url: str):
+        self.driver.get(url)
+
+    def enter_username(self, username: str):
+        element_web = self.driver.find_element(*self.USER_INPUT) ## ('id', 'user')
+        element_web.clear()
+        element_web.send_keys(username)
+
+    def enter_password(self, password: str):
+        element_web = self.driver.find_element(*self.PASS_INPUT)
+        element_web.clear()
+        element_web.send_keys(password)
+
+    def click_login(self):
+        btn_login = self.driver.find_element(*self.LOGIN_BUTTON)
+        btn_login.click()
+
+    def get_feedback_test(self) -> str:
+        return self.driver.find_element(*self.FEEDBACK).text
+    
